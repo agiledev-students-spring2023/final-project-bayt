757a8ae45b01e3c93c452f3327553b519b895bdb
dolf321 <dolfzigler@hotmail.com>
2023-03-03 15:06:19 -0500

    Set up task component with some basic information

---
+++ front-end/src/pages/task_component.jsx
@@ -0,0 +1,43 @@
+// Login Page
+
+import React from 'react';
+
+import "../css/Login.css";
+
+/**
+ * A React component that represents the task box for listing tasks
+ * @param {string} room location taks is assigned to
+ * @param {string} name task name
+ * @param {string} assigned person task is assigned to, if any
+ * @returns The task component as highlighted below
+ */
+const task_component = ({ room, name, assigned }) => { {
+    return (
+        <div className="task_component">
+            
+        </div>
+        // <div id='outer'>
+        //     <div className='logoContainer'>
+        //         <div className='logo'>
+        //             <p>LOGO</p>
+        //         </div>
+        //     </div>
+
+        //     <div className='inputArea'>
+        //         <input id='usernameInput' type="text" name='username' placeholder='USERNAME'/>
+        //         <input id='passwordInput' type="password" name='housecode' placeholder='HOUSE CODE' />
+        //         <input id='loginBtn' type="submit" value='Login' />
+        //     </div>
+
+        //     <div className='signupArea'>
+        //         <button id='signupBtn'>Sign Up</button>
+        //     </div>
+
+        //     <footer className='loginFooter'>
+        //         <h6>© 2023 Bayt</h6>
+        //     </footer>
+        // </div>
+    );
+}
+
+export default task_component;
