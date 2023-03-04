841c1c1e6787928f6139b30d578a57d004f58d02
jojo1042 <jojo.yang1042@gmail.com>
2023-03-04 16:32:29 -0500

    adding footer files
    rebasing with new master

---
+++ front-end/src/pages/Footer.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// import '../css/Footer.css'
+
+// const Footer = props => {
+//   return (
+//     <footer className="Footer-footer">
+//       <Button variant="contained">test</Button>
+//     </footer>
+//   )
+// }
