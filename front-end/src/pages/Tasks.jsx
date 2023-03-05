2a361f694ed95dd99c8d6ac1f31a504dba701565
dolf321 <dolfzigler@hotmail.com>
2023-03-05 18:40:26 -0500

    Added numerous features discussed in ux-design

--- front-end/src/pages/Tasks.jsx
+++ front-end/src/pages/Tasks.jsx
@@ -1,92 +1,183 @@
 // Login Page
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "../css/tasks.css";
-import  TaskComponent from "../components/Task_Component.jsx"
-import CircularProgress from '@mui/material/CircularProgress';
-const task_json = [{"id":{"$oid":"6404d2a0f032391df000185a"},"task_name":"clean bathroom","description":"wipe the bed","room":"Providencia","assignee":"Letty","due_time":{"$date":{"$numberLong":16472100091400}},"repeat":1}, {"id":{"$oid":"6404d2a0f032391df000185b"},"task_name":"buy an air fryer","description":"clean properly this time","room":"Gorkha","assignee":"Rosette","due_time":{"$date":{"$numberLong":164619988300}},"repeat":1}, {"id":{"$oid":"6404d2a0f032391df000185c"},"task_name":"clean bathroom","description":"wipe the bed","room":"Playón Chico","assignee":"Karol","due_time":{"$date":{"$numberLong":164646661400}},"repeat":1}, {"id":{"$oid":"6404d2a0f032391df000185d"},"task_name":"cook spaghetti","description":"wake up dog","room":"Fitiuta Village","assignee":"Idette","due_time":{"$date":{"$numberLong":164639990600}},"repeat":1}, {"id":{"$oid":"6404d2a0f032391df000185e"},"task_name":"cook spaghetti","description":"wipe the bed","room":null,"assignee":"Anya","due_time":{"$date":{"$numberLong":164716927600}},"repeat":1}, {"id":{"$oid":"6404d2a0f032391df000185f"},"task_name":"clean bathroom","description":"wake up dog","room":"Sioux Lookout","assignee":"Ramsey","due_time":{"$date":{"$numberLong":164696181700}},"repeat":1}];
-const Tasks = props =>  {
-    const [data, setData] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-    const [error, setError] = useState('');
+import TaskComponent from "../components/Task_Component.jsx";
+import CircularProgress from "@mui/material/CircularProgress";
 
-    let prev_day = '';
-    const TaskDay = task => {
-        const task_date =  new Date(task.due_time.$date.$numberLong);
-        let day = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(task_date);
-        const diff = task_date.valueOf() - Date.now().valueOf();
+//TESTING MODE: This is to toggle between API calls and hardcoded JSON data
+const testing_mode = true;
 
-        //Display in Month, day, year format if greater than a week
-        if(diff>=604800000) {
-            day = new Intl.DateTimeFormat("en-US", {month: "long", day: "numeric", year: "numeric"}).format(task_date);
-        } 
+const Tasks = (props) => {
+  const [data, setData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState("");
 
-        if(day!==prev_day) {
-            prev_day = day
-            return (
-                <h2 className="day_title">{day}</h2>
-            );
-        }
-
-    }
-
-    //Sorting the dates of DATA elems
-    const sortComparator = (a,b) => {
-        // Sort by date 
-        return a.due_time.$date.$numberLong-b.due_time.$date.$numberLong;
-    }
-
-    const fetchData = () => {
-        setData(task_json)
-        data.sort(sortComparator)
-        setLoaded(true)
-        // axios
-        // .get(`${process.env.REACT_APP_API_TASKS}`)
-        // .then(response => {
-        //     // axios bundles up all response da ta in response.data property
-        //     // const data = response.data
-        //     // data.sort(sortComparator)
-        //     setData(data)
-        // })
-        // .catch(err => {
-        //     setError(err)
-        // })
-        // .finally(() => {
-        //     setLoaded(true)
-        // })
-    }
-
-    useEffect(() => {
-        // fetch messages this once
-        fetchData()
-    
-        // set a timer to load data from server every n seconds
-        // const intervalHandle = setInterval(() => {
-        //   fetchData()
-        // }, 5000)
-    
-        // return a function that will be called when this component unloads
-        return e => {
-          // clear the timer, so we don't still load messages when this component is not loaded anymore
-        //   clearInterval(intervalHandle)
-        }
-      }) 
-
-    return (
-        <div className='task_box'>
-            {error && <p>{error}</p>}
-            {!loaded && <CircularProgress className="loading_icon"/>}
-            {data.map((task_data) => {
-                return(
-                    <div key={task_data.id.$oid}>
-                        {TaskDay(task_data)}
-                        <TaskComponent title={task_data.task_name} room={task_data.room} assigned={task_data.assignee}/>
-                    </div>
-                );
-            })}
-            
-        </div>
+  let prev_day = "";
+  const TaskDay = (task) => {
+    const task_date = new Date(task.due_time.$date.$numberLong);
+    let day = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+      task_date
     );
-}
+    const diff = task_date.valueOf() - Date.now().valueOf();
 
+    //Display in Month, day, year format if greater than a week
+    if (Math.abs(diff) >= 604800000) {
+      day = new Intl.DateTimeFormat("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      }).format(task_date);
+    }
+
+    if (day !== prev_day) {
+      prev_day = day;
+      return (
+        <>
+          <div className="line"></div>
+          <h2 className="day_title">{day}</h2>
+        </>
+      );
+    }
+  };
+
+  //Comparator for the sorting by the dates of API DATA elems
+  const sortComparator = (a, b) => {
+    return a.due_time.$date.$numberLong - b.due_time.$date.$numberLong;
+  };
+
+  const fetchData = () => {
+    if (testing_mode) {
+      setData(task_json);
+      task_json.sort(sortComparator);
+      setLoaded(true);
+    } else {
+      axios
+        .get(`${process.env.REACT_APP_API_TASKS}`)
+        .then((response) => {
+          // axios bundles up all response da ta in response.data property
+          const data = response.data;
+          data.sort(sortComparator);
+          setData(data);
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setLoaded(true);
+        });
+    }
+  };
+
+  useEffect(() => {
+    // fetch messages this once
+    fetchData();
+
+    // set a timer to load data from server every n seconds
+    const intervalHandle = setInterval(() => {
+      fetchData();
+    }, 5000);
+
+    // return a function that will be called when this component unloads
+    return (e) => {
+      // clear the timer, so we don't still load messages when this component is not loaded anymore
+      clearInterval(intervalHandle);
+    };
+  }, []);
+
+  return (
+    <div className="task_box">
+      {error && <p>{error}</p>}
+      {!loaded && <CircularProgress className="loading_icon" />}
+      {data.map((task_data) => {
+        return (
+          <div key={task_data.id.$oid}>
+            {TaskDay(task_data)}
+            <TaskComponent
+              title={task_data.task_name}
+              room={task_data.room}
+              assigned={task_data.assignee}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default Tasks;
+
+//MOCK DATA
+let task_json = [
+    {
+      id: { $oid: "6403e5c9f032391df0001828" },
+      task_name: "wipe floor in kitchen",
+      description: "clean properly this time",
+      room: "Vipingo Estate",
+      assignee: "Rheta",
+      due_time: { $date: { $numberLong: 165676748700 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df0001829" },
+      task_name: "call maintenance",
+      description: "clean properly this time",
+      room: "Mahanoro",
+      assignee: "Benedict",
+      due_time: { $date: { $numberLong: 166807471100 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182a" },
+      task_name: "call maintenance",
+      description: "take dog on walk",
+      room: "Paso Caballos",
+      assignee: "Vergil",
+      due_time: { $date: { $numberLong: 165998506900 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182b" },
+      task_name: "call maintenance",
+      description: "clean properly this time",
+      room: "Ponta Grossa",
+      assignee: "Eugenius",
+      due_time: { $date: { $numberLong: 166918952900 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182c" },
+      task_name: "take out trash",
+      description: "take dog on walk",
+      room: "Eureka",
+      assignee: "Gilemette",
+      due_time: { $date: { $numberLong: 167079582300 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182d" },
+      task_name: "wipe floor in kitchen",
+      description: "clean properly this time",
+      room: "Burns",
+      assignee: "Chrissie",
+      due_time: { $date: { $numberLong: 165918810000 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182e" },
+      task_name: "call maintenance",
+      description: "wake up dog",
+      room: "Omsk",
+      assignee: "Obadias",
+      due_time: { $date: { $numberLong: 165777791000 } },
+      repeat: 1,
+    },
+    {
+      id: { $oid: "6403e5c9f032391df000182f" },
+      task_name: "cook spaghetti",
+      description: "clean properly this time",
+      room: "Poprad",
+      assignee: "Kelcie",
+      due_time: { $date: { $numberLong: 165402530900 } },
+      repeat: 1,
+    },
+  ];
