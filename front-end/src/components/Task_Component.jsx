bdc25e076ad4f4998b3da37c15d045c7307dedd8
dolf321 <dolfzigler@hotmail.com>
2023-03-13 23:33:37 -0400

    Added checkbox to task component

--- front-end/src/components/Task_Component.jsx
+++ front-end/src/components/Task_Component.jsx
@@ -1,12 +1,16 @@
 // Login Page
 
-import React from 'react';
+import React, { useEffect } from "react";
 import "../css/task_component.css";
 
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import RoomIcon from '@mui/icons-material/Room';
-import PendingActionsIcon from '@mui/icons-material/PendingActions';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import {
+  Room,
+  PendingActions,
+  ArrowForwardIos,
+  AlternateEmail,
+} from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
+
 /**
  * A React component that represents the task box for listing tasks
  * @param {string} room location taks is assigned to
@@ -14,31 +18,33 @@ import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
  * @param {string} assigned person task is assigned to, if any
  * @returns The task component as highlighted below
  */
-const TaskComponent = ({ title, room, assigned }) => {
+const TaskComponent = ({id, title, room, assigned, completed, SelectHandler}) => {
+  const [isChecked, setIsChecked] = React.useState(false);
 
-    return (
+  return (
+    <div className={"task_box_comp "+(completed?"complete":"incomplete")}>
+      <Checkbox className="checkbox" checked={isChecked && !completed} onChange={e => {setIsChecked(e.target.checked); SelectHandler(id, e.target.checked && !completed)}}/>
+      <div className={"task_component "+(isChecked && !completed ?"selected":"deselected")}>
+        <div className="task_information">
+          <div className="task_name">
+            <PendingActions className="task_text_icon" /> {title}
+          </div>
 
-        <div className="task_component">
-            
-            <div className="task_information">
-                <div className="task_name">
-                    <PendingActionsIcon className="task_text_icon"/> {title}
-                </div>
-
-                <div className="task_text"> 
-                    <RoomIcon className="task_text_icon"/> {room}
-                </div>
-
-                <div className="task_text"> 
-                    <AlternateEmailIcon className="task_text_icon"/> {assigned}
-                </div>
+          {room && (
+            <div className="task_text">
+              <Room className="task_text_icon" /> {room}
             </div>
-       
-            <ArrowForwardIosIcon className="next_button"/>
+          )}
 
+          <div className="task_text">
+            <AlternateEmail className="task_text_icon" /> {assigned}
+          </div>
         </div>
 
-    );
-}
+        <ArrowForwardIos className="next_button" />
+      </div>
+    </div>
+  );
+};
 
 export default TaskComponent;
