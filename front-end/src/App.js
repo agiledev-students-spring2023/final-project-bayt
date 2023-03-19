1d704cae7ef044f48df1c5df8e9476cf5eda395b
zion-off <ajz317@nyu.edu>
2023-03-19 19:58:57 -0400

    tried to rebase

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,43 +1,28 @@
 import './App.css';
-import './css/branding.css'
+import './css/branding.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from './pages/Login';
 import Tasks from './pages/Tasks';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-
-// TODOS:
-// 1. The index page is login page right now. Should be set to home page once we have user authentication
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from './pages/Login';
 import Finances from './pages/finances';
 import Alerts from './pages/alerts';
-
-// TODOS:
-// 1. The index page is login page right now. Should be set to home page once we have user authentication
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
-        <div>
-        <Header />
-      <BrowserRouter>
-          <Routes>
-            <Route index path="/" element={<Login />}></Route>
-            <Route index path="/tasks" element={<Tasks />}></Route>
-            <Route index path="/home" element={<Home />}></Route>
-            <Route index path="/Profile" element={<Profile/>}></Route>
-          </Routes>
-      </BrowserRouter>
-          <Routes>
-            <Route index path="/" element={<Login />}></Route>
-            <Route index path="/pages/finances" element={<Finances />}></Route>
-            <Route index path="/pages/alerts" element={<Alerts />}></Route>
-          </Routes>
-        </div>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route index path="/tasks" element={<Tasks />} />
+          <Route index path="/home" element={<Home />} />
+          <Route index path="/Profile" element={<Profile />} />
+          <Route index path="/finances" element={<Finances />} />
+          <Route index path="/alerts" element={<Alerts />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
