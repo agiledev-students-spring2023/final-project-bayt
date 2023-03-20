386e2985d9701c8a36a9c30bfe9ab6986ba038f9
zion-off <ajz317@nyu.edu>
2023-03-19 23:35:04 -0400

    rami

--- front-end/src/pages/Footer.jsx
+++ front-end/src/pages/Footer.jsx
@@ -1,37 +1,63 @@
-import React from 'react'
+import React from "react";
 
-import '../css/Footer.css'
-import { Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
-import HomeIcon from '@mui/icons-material/Home'
-import NotificationsIcon from '@mui/icons-material/Notifications'
-import TuneIcon from '@mui/icons-material/Tune'
+import "../css/Footer.css";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import HomeIcon from "@mui/icons-material/Home";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import TuneIcon from "@mui/icons-material/Tune";
 
-const Footer = props => {
-
-  const variant = "text"
+const Footer = (props) => {
+  const variant = "text";
 
   return (
-    <footer className="footer">
-      <Button variant={variant} component={Link} to="/tasks" startIcon={<FormatListBulletedIcon />}>
-        Tasks
-      </Button>
-      <Button variant={variant} component={Link} to="/finances" startIcon={<AttachMoneyIcon />}>
-        Finances
-      </Button>
-      <Button variant={variant} component={Link} to="/home" startIcon={<HomeIcon />}>
-        Home
-      </Button>
-      <Button variant={variant} component={Link} to="/alerts" startIcon={<NotificationsIcon />}>
-        Alerts
-      </Button>
-      <Button variant={variant} component={Link} to="/settings" startIcon={<TuneIcon />}>
-        Settings
-      </Button>
-    </footer>
-  )
-}
+    <div className="page-body">
+      <footer className="footer">
+        <Button
+          variant={variant}
+          component={Link}
+          to="/tasks"
+          startIcon={<FormatListBulletedIcon />}
+        >
+          Tasks
+        </Button>
+        <Button
+          variant={variant}
+          component={Link}
+          to="/finances"
+          startIcon={<AttachMoneyIcon />}
+        >
+          Finances
+        </Button>
+        <Button
+          variant={variant}
+          component={Link}
+          to="/home"
+          startIcon={<HomeIcon />}
+        >
+          Home
+        </Button>
+        <Button
+          variant={variant}
+          component={Link}
+          to="/alerts"
+          startIcon={<NotificationsIcon />}
+        >
+          Alerts
+        </Button>
+        <Button
+          variant={variant}
+          component={Link}
+          to="/settings"
+          startIcon={<TuneIcon />}
+        >
+          Settings
+        </Button>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
