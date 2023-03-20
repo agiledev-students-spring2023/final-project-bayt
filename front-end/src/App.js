8841a18de37876dea2d45eb2a6807babab354b17
zion-off <ajz317@nyu.edu>
2023-03-19 20:04:36 -0400

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
 import './css/branding.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from './pages/Login';
 import Tasks from './pages/Tasks';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Header from './pages/Header';
 import Finances from './pages/finances';
 import Alerts from './pages/alerts';
-import Header from './header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
         <Routes>
-          <Route index path="/" element={<Login />} />
-          <Route index path="/tasks" element={<Tasks />} />
-          <Route index path="/home" element={<Home />} />
-          <Route index path="/Profile" element={<Profile />} />
-          <Route index path="/finances" element={<Finances />} />
-          <Route index path="/alerts" element={<Alerts />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/finances" element={<Finances />} />
+          <Route path="/alerts" element={<Alerts />} />
         </Routes>
       </BrowserRouter>
     </div>
