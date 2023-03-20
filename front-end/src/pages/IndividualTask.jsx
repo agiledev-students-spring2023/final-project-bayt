b1a26d28f3f073c46264f4d7bd38adf562a23a5f
ccczy-czy <zc2122@nyu.edu>
2023-03-20 11:41:27 -0400

    finish add task page

--- front-end/src/pages/IndividualTask.jsx
+++ front-end/src/pages/IndividualTask.jsx
@@ -10,11 +10,12 @@ import Select from '@mui/material/Select';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 import '../css/IndividualTask.css';
 
-import Header from '../components/Header';
+// import Header from '../components/Header';
 import Footer from './Footer.jsx';
 
 const defaultValues = {
@@ -22,18 +23,36 @@ const defaultValues = {
     room:"",
     assign:"",
     repeat: "",
-    date: null,
+    userDate: new Date(),
+    taskDescription: "",
 };
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#81b29a',
+        contrastText: '#fff',
+      },
+    },
+});
 
 function IndividualTask(props) {
     const [formValues, setFormValues] = useState(defaultValues)
 
     const handleInputChange = (e, date) => {
-        const { name, value } = e.target;
+        if (e == null && date != null) {
             setFormValues({
-            ...formValues,
-            [name]: value,
-        });
+                ...formValues,
+                userDate: date['$d']
+            });
+        }
+        else {
+            const { name, value } = e.target;
+                setFormValues({
+                ...formValues,
+                [name]: value,
+            });
+        }
     };
 
     const handleSubmit = (e) => {
@@ -42,52 +61,56 @@ function IndividualTask(props) {
     };
 
     return (
-        <>
-            <Header></Header>
-            <form onSubmit={handleSubmit}>
-                <Box sx={{m: 3, maxWidth: '100%'}}>
-                    <TextField sx={{mb: 2}} variant="standard" fullWidth id="taskTitle-input" name="taskTitle" label="Task Title" type="text" value={formValues.taskTitle} onChange={handleInputChange} />
-                    
-                    <FormControl variant="standard" sx={{mb: 2, width: '100%'}}>
-                        <InputLabel id="room-label">Select Room</InputLabel>
-                        <Select name="room" labelId="room-label" id="room-select-helper" value={formValues.room} label="room" onChange={handleInputChange}>
-                            <MenuItem value={'bathroom'}>Bathroom</MenuItem>
-                            <MenuItem value={'kitchen'}>Kitchen</MenuItem>
-                            <MenuItem value={'livingroom'}>Living Room</MenuItem>
-                        </Select>
-                    </FormControl>
+        <div>
+            {/* <Header></Header> */}
+            <form className='taskFormContainer' onSubmit={handleSubmit}>
+                <ThemeProvider theme={theme}>
+                    <Box sx={{m: 3, mt: 0, pt: 3, maxWidth: '100%'}}>
+                        <TextField required sx={{mb: 2}} variant="standard" fullWidth id="taskTitle-input" name="taskTitle" label="Task Title" type="text" value={formValues.taskTitle} onChange={handleInputChange} />
 
-                    <FormControl variant="standard" sx={{mb: 2, width: '100%'}}>
-                        <InputLabel id="assign-label">Assign To</InputLabel>
-                        <Select name="assign" labelId="assign-label" id="assign-select-helper" value={formValues.assign} label="assign" onChange={handleInputChange}>
-                            <MenuItem value={'tom'}>Tom</MenuItem>
-                            <MenuItem value={'james'}>James</MenuItem>
-                            <MenuItem value={'aaron'}>Aaron</MenuItem>
-                        </Select>
-                    </FormControl>
+                        <TextField sx={{mb: 2}} fullWidth required id="taskDescription" name='taskDescription' label="Enter Task Description" multiline rows={4} defaultValue={formValues.taskDescription} variant="standard" onChange={handleInputChange}/>
+                        
+                        <FormControl required variant="standard" sx={{mb: 2, width: '100%'}}>
+                            <InputLabel id="room-label">Select Room</InputLabel>
+                            <Select required name="room" labelId="room-label" id="room-select-helper" value={formValues.room} label="room" onChange={handleInputChange}>
+                                <MenuItem value={'bathroom'}>Bathroom</MenuItem>
+                                <MenuItem value={'kitchen'}>Kitchen</MenuItem>
+                                <MenuItem value={'livingroom'}>Living Room</MenuItem>
+                            </Select>
+                        </FormControl>
 
-                    <FormControl variant="standard" sx={{mb: 2, width: '100%'}}>
-                        <InputLabel id="repeat-label">Repeat Every</InputLabel>
-                        <Select name="repeat" labelId="repeat-label" id="repeat-select-helper" value={formValues.repeat} label="repeat" onChange={handleInputChange}>
-                            <MenuItem value={'never'}>Never</MenuItem>
-                            <MenuItem value={'everyday'}>Every Day</MenuItem>
-                            <MenuItem value={'everyweek'}>Every Week</MenuItem>
-                            <MenuItem value={'every2week'}>Every 2 Weeks</MenuItem>
-                            <MenuItem value={'everymonth'}>Every Month</MenuItem>
-                            <MenuItem value={'everyyear'}>Every Year</MenuItem>
-                        </Select>
-                    </FormControl>
+                        <FormControl required variant="standard" sx={{mb: 2, width: '100%'}}>
+                            <InputLabel id="assign-label">Assign To</InputLabel>
+                            <Select required name="assign" labelId="assign-label" id="assign-select-helper" value={formValues.assign} label="assign" onChange={handleInputChange}>
+                                <MenuItem value={'tom'}>Tom</MenuItem>
+                                <MenuItem value={'james'}>James</MenuItem>
+                                <MenuItem value={'aaron'}>Aaron</MenuItem>
+                            </Select>
+                        </FormControl>
 
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker hintText="Select the date" onChange={handleInputChange} />
-                    </LocalizationProvider>
+                        <FormControl required variant="standard" sx={{mb: 2, width: '100%'}}>
+                            <InputLabel id="repeat-label">Repeat Every</InputLabel>
+                            <Select required name="repeat" labelId="repeat-label" id="repeat-select-helper" value={formValues.repeat} label="repeat" onChange={handleInputChange}>
+                                <MenuItem value={'never'}>Never</MenuItem>
+                                <MenuItem value={'everyday'}>Every Day</MenuItem>
+                                <MenuItem value={'everyweek'}>Every Week</MenuItem>
+                                <MenuItem value={'every2week'}>Every 2 Weeks</MenuItem>
+                                <MenuItem value={'everymonth'}>Every Month</MenuItem>
+                                <MenuItem value={'everyyear'}>Every Year</MenuItem>
+                            </Select>
+                        </FormControl>
 
-                    <Button variant="outlined" fullWidth={true} endIcon={<LibraryAddIcon />} color="primary" type="submit">Save</Button>
-                </Box>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker required label="Select Date" sx={{mb: 2, width: '100%'}} componentsProps={{ textField: { required: true, fullWidth: true } }} onChange={date => handleInputChange(null, date)} />
+                        </LocalizationProvider>
+                        
+                        <Button variant="contained" fullWidth={true} endIcon={<LibraryAddIcon />} color="primary" type="submit">Save</Button>
+                    </Box>
+                </ThemeProvider>
             </form>
 
             <Footer></Footer>
-        </>
+        </div>
     )
 }
 
