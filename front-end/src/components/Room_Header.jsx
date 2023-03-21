b45cd5b7057a01d3f92b43fd3b6e9dc6a052aa99
ccczy-czy <zc2122@nyu.edu>
2023-03-20 20:01:02 -0400

    add back button

--- front-end/src/components/Room_Header.jsx
+++ front-end/src/components/Room_Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import ArrowBackIosNewTwoToneIcon from '@mui/icons-material/ArrowBackIosNewTwoTone';
+import { useNavigate } from 'react-router-dom';
 
 const separateWords = str => {
     let newStr = "";
@@ -18,11 +19,17 @@ const separateWords = str => {
 };
 
 function RoomHeader(props) {
+    const navigate = useNavigate();
+
+	const handleClick = () => {
+		return navigate(-1);
+	}
+
     return (
         <Box sx={{ position: 'fixed', left: '0', top: '0', zIndex: '1', width: '100%' }}>
             <Grid container sx={{ alignItems: 'center', backgroundColor: '#f2cc8f' }}>
                 <Grid item xs={2} sx={{ p: 1, textAlign: 'left' }}>
-                    <ArrowBackIosNewTwoToneIcon sx={{color: '#fff', backgroundColor: '#3d405b', borderRadius: '0.4rem' }} style={{fontSize: '3rem'}} ></ArrowBackIosNewTwoToneIcon>
+                    <ArrowBackIosNewTwoToneIcon onClick={handleClick} sx={{color: '#fff', backgroundColor: '#3d405b', borderRadius: '0.4rem' }} style={{fontSize: '3rem'}} ></ArrowBackIosNewTwoToneIcon>
                 </Grid>
 
                 <Grid item xs={8} sx={{ textAlign: 'center' }}>
