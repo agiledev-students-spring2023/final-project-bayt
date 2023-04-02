e2ff108b80d6bfc53473350f0ba40a414da95f1b
zion-off <ajz317@nyu.edu>
2023-04-02 18:05:20 -0400

    almost done with finances

--- back-end/routes/finances.routes.js
+++ back-end/routes/finances.routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const finances_controller = require('../controllers/finances.controller.js');
-
 const router = express.Router();
+const finances_controller = require("../controllers/finances.controller.js");
 
-router.get("/transactions", finances_controller.getAllTransactions);
-router.post("/transactions", finances_controller.addTransaction);
+router.get("/", finances_controller.getAllTransactions);
+router.post("/", finances_controller.addTransaction);
 
 module.exports = router;
- 
