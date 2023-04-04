21a755e0782778a1dca6023eb8da88c2ff8e3e25
dolf321 <dolfzigler@hotmail.com>
2023-04-03 21:39:43 -0400

    Refactored, improved file structure, improved resp

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,10 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const bodyParser = require('body-parser');
+const taskRouter = require('./routes/task.route.js');
 const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 const fs = require('fs');
-const taskRouter = require('./routes/task.routes.js');
-const profRouter = require('./routes/prof.routes.js');
+const profRouter = require('./routes/prof.route.js');
 const financesRouter = require('./routes/finances.routes.js');
 const app = express() // instantiate an Express object
 
