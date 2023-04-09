228e6ad8c5953e3a3b99bda69a70592aaf35ebde
dianaYepes <day256@nyu.edu>
2023-04-09 04:30:52 -0400

    add-Members routing added

--- back-end/src/routes/addmembers.route.js
+++ back-end/src/routes/addmembers.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const addMembers_controller = require("../controllers/addmembers.controller.js");
 
-//router.get("/", addMembers_controller.getAllTransactions);
-//router.post("/", addMembers_controller.addTransaction);
+router.post("/", addMembers_controller.saveUser);
 
 module.exports = router;
