15bec29326128b53001994a655a4dd7bcf607330
dianaYepes <day256@nyu.edu>
2023-04-09 10:44:11 -0400

    signup routing

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -6,15 +6,16 @@ import HouseCodeForm from '../components/HouseCodeForm';
 import CreateProfileForm from '../components/CreateProfileForm';
 import { useNavigate } from 'react-router-dom';
 import "../index.css";
+import axios from 'axios';
 
 import '../css/Signup.css';
 
-function getStepContent(step) {
+function getStepContent(step, handleHouseCodeData, handleProfileData) {
   switch (step) {
     case 0:
-      return <HouseCodeForm />;
+      return <HouseCodeForm handleHouseCodeData={handleHouseCodeData} />;
     case 1:
-      return <CreateProfileForm />;
+      return <CreateProfileForm handleProfileData={handleProfileData} />;
     default:
       throw new Error('Unknown step');
   }
@@ -36,6 +37,8 @@ const theme = createTheme({
 export default function Checkout() {
   const navigate = useNavigate();
 
+  const [houseCodeData, setHouseCodeData] = React.useState({});
+  const [profileData, setProfileData] = React.useState({});
   const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
@@ -50,8 +53,27 @@ export default function Checkout() {
     return navigate(-1);
   }
 
-  const handleFinish = () => {
-    return navigate('/home');
+  const handleFinish = async() => {
+    // Make post request here with houseCodeData and profileData
+    try {
+      const response = await axios.post('/signup', {
+        houseCodeData: houseCodeData,
+        profileData: profileData
+      });
+      console.log(response.data);
+      navigate('/home');
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleHouseCodeData = (data) => {
+    setHouseCodeData(data);
+  }
+
+  const handleProfileData = (data) => {
+    setProfileData(data);
   }
 
   return (
@@ -61,7 +83,7 @@ export default function Checkout() {
           <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
             {
               <>
-                {getStepContent(activeStep)}
+                {getStepContent(activeStep, handleHouseCodeData, handleProfileData)}
                 <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', mt: 7 }}>
                   {activeStep !== 0 ? (<button onClick={handleBack}>Back</button>) : (<button onClick={handleNavigate} >Back</button>)}
                   {activeStep !== 1 ? (<button onClick={handleNext}>Next</button>) : (<button onClick={handleFinish} >Finish</button>)}
