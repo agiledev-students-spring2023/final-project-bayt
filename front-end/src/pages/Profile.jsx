6357c18a81a8ef63d0856a902e4dbcac5a0918e2
zion-off <ajz317@nyu.edu>
2023-04-10 21:32:44 -0400

    changed profile edit button

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,50 +1,45 @@
-import { useState} from "react";
+import { useState } from "react";
 import ProfilePic from "../components/Profile_pic_component";
 import ProfInfo from "../components/Profile_info_component";
 import "../css/Profile_pic_component.css";
-import Header from './Header';
-import Footer from './Footer';
-import '../css/Profile.css';
-import * as React from 'react';
-import axios from 'axios';
-import "../index.css"
+import Header from "./Header";
+import Footer from "./Footer";
+import "../css/Profile.css";
+import * as React from "react";
+import axios from "axios";
+import "../index.css";
 
-//editable Name part of profile.  It renders and updates each time user changes it. 
+//editable Name part of profile.  It renders and updates each time user changes it.
 const NameInfo = () => {
-
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [isEditing, setIsEditing] = useState(false);
-  const [tempName, setTempName] = useState('');
-
+  const [tempName, setTempName] = useState("");
 
   React.useEffect(() => {
     axios
-        .get(`/api/Profile`)
-        .then(response => {
-         setName(response.data.username);   
-        })
-    .catch (err => {
-    console.log(err);
-    })
-}, [])
-
+      .get(`/api/Profile`)
+      .then((response) => {
+        setName(response.data.username);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const handleNameClick = () => {
     setIsEditing(true);
     setTempName(name);
   };
 
-
   const handleNameChange = (event) => {
     setTempName(event.target.value);
   };
 
-
   const handleNameKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       setIsEditing(false);
 
-      if (tempName.trim() !== '') {
+      if (tempName.trim() !== "") {
         setName(tempName.trim());
         axios
           .put(`/api/Profile`, { username: tempName.trim() })
@@ -54,9 +49,7 @@ const NameInfo = () => {
           .catch((error) => {
             console.log(error);
           });
-      } 
-
-      else {
+      } else {
         setTempName(name);
       }
     }
@@ -64,7 +57,7 @@ const NameInfo = () => {
 
   const handleNameBlur = () => {
     setIsEditing(false);
-    if (tempName.trim() !== '') {
+    if (tempName.trim() !== "") {
       setName(tempName.trim());
       axios
         .put(`/api/Profile`, { username: tempName.trim() })
@@ -74,14 +67,11 @@ const NameInfo = () => {
         .catch((error) => {
           console.log(error);
         });
-    } 
-
-    else {
+    } else {
       setTempName(name);
     }
-
   };
-  
+
   return (
     <div className="profile-container">
       {isEditing ? (
@@ -101,33 +91,23 @@ const NameInfo = () => {
       )}
     </div>
   );
-    
-}
-
-
-
+};
 
 //replace h1 with header and delete from css
 const Profile = () => {
-
-
-
-
-
-    return (
+  return (
     <>
-      <Header title="Profile"/>
+      <Header title="Profile" />
       <div>
         <div className="outer">
-            <ProfilePic/>
-            <NameInfo/>
-            <ProfInfo/>
+          <ProfilePic />
+          <NameInfo />
+          <ProfInfo />
         </div>
       </div>
-      <Footer/>
-    </>  
-    );
+      <Footer />
+    </>
+  );
 };
 
-export default Profile
-
+export default Profile;
