c12fa089b40a9871617b7acf93725d158776f417
dolf321 <rami.r.richani02@hotmail.com>
2023-04-13 19:20:58 -0400

    ADD task list schema

---
+++ back-end/src/models/task.list.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const usersModel = require('./users.model');
+const Schema = mongoose.Schema
+
+// Schema for tasks matching this json object format:
+// {
+//     "id": {
+//         "$oid": "642e3662fc13ae490678cb3a"
+//     },
+//     "task_name": "call maintenance",
+//     "description": "clean properly this time",
+//     "room": "Ilaka",
+//     "assignee": "badbunny",
+//     "due_time": {
+//         "$date": {
+//             "$numberLong": 164707834700
+//         }
+//     },
+//     "complete": true,
+//     "repeat": 1
+// }
+
+const TaskSchema = new Schema({
+    task_name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    room: {
+        type: String,
+        required: true
+    },
+    assignee: { // this should be an array of type user and populated user's
+        type: mongoose.ObjectId,
+        ref: usersModel
+    },
+    due_time: { // MongoDB date type
+        type: Date,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        required: true
+    },
+    repeat: {
+        type: Number,
+        required: true
+    },
+});
+
+module.exports = Task = mongoose.model('task', TaskSchema);
