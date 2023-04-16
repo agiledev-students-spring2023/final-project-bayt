1f1bb1a1cf5bef573ae3e052bb9f74fd74596cf1
dolf321 <rami.r.richani02@hotmail.com>
2023-04-16 13:28:45 -0400

    ADD task connection to database

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 
 // import and instantiate mongoose
-const mongoose = require("mongoose") 
-// require('dotenv/config');
+const mongoose = require("mongoose")
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
 
 // // connect to database
-// mongoose
-//   .connect(`${process.env.DB_CONNECTION_STRING}`)
-//   .then(data => console.log(`Connected to MongoDB`))
-//   .catch(err => console.error(`Failed to connect to MongoDB: ${err}`))
+if (process.env.NODE_ENV === 'test') {
+    mongoose
+        .connect(`${process.env.DB_CONNECTION_STRING}`)
+        .then(data => console.log(`Connected to MongoDB`))
+        .catch(err => console.error(`Failed to connect to MongoDB: ${err}`))
+}
 
 const taskRouter = require('./routes/task.route.js');
 const profRouter = require('./routes/prof.route.js');
@@ -39,15 +41,15 @@ app.use('/finances', financesRouter);
 //parse alerts data
 app.use('/alerts', alertsRouter);
 //parse settings data
-app.use('/settings',settingsRouter);
+app.use('/settings', settingsRouter);
 // parse login data
 app.use('/login', loginRouter);
 //parse addMembers data
-app.use('/addMembers',addMembersRouter);
+app.use('/addMembers', addMembersRouter);
 // parse home data
 app.use('/home', homeRouter);
 // parse signup data
-app.use(`/signup`,signupRouter);
+app.use(`/signup`, signupRouter);
 
 // export the express app we created to make it available to other modules
 module.exports = app
--- back-end/src/services/task.service.js
+++ back-end/src/services/task.service.js
@@ -1,37 +1,83 @@
-// SERVICE HARDCODED(AT LEAST TEMPORARILY) TASK DATA
+// // SERVICE HARDCODED(AT LEAST TEMPORARILY) TASK DATA
+let getTasks, getTask, CreateTask, UpdateTask, RemoveTask;
 let task_json = require('../json/tasklist.json')
+// load the dataabase models we want to deal with
+const Task = require('../models/task.list.model.js');
 
-async function getTasks() {
-  return task_json;
+//Import the task
+if (process.env.NODE_ENV === 'production') {
+  getTasks = async () => {
+    return Task.find({});
+  };
+
+  getTask = async (task_id) => {
+    return Task.findById(task_id);
+  };
+
+  // make sure task doesnt exist in database then add the task to the Task
+  createTask = async (task_data) => {
+    const task_query = await getTask(task_data.id.$oid);
+    if (task_query !== null) throw new Error("Task already exists");
+    await Task.create(task_data);
+    return "Task created successfully";
+  };
+
+  // find and update the task MongoDB document based off task_data json object
+  updateTask = async (task_id, task_data) => {
+    Task.findByIdAndUpdate(task_id, task_data).then((updatedTask) => {
+      if (!updatedTask) {
+        throw new Error("Error in updating task");
+      }
+      return "Task updated successfully";
+    }).catch(() => {
+      throw new Error("DB Error in updating task");
+    });
+  };
+
+  // find and remove the task MongoDB document based off task_id
+  removeTask = async (task_id) => {
+    Task.findByIdAndDelete(task_id).then((deletedTask) => {
+      if (!deletedTask) {
+        throw new Error("Error in deleting task");
+      }
+      return "Task deleted successfully";
+    }).catch(() => {
+      throw new Error("DB Error in deleting task");
+    });
+  };
+} else {
+  getTasks = async () => {
+    return task_json;
+  };
+
+  getTask = async (task_id) => {
+    return task_json.find((task) => task.id.$oid === task_id);
+  };
+
+  // add task data to task data json array if it does not exist
+  createTask = async (task_data) => {
+    const task = await getTask(task_data.id.$oid);
+    if (task !== undefined) throw new Error("Task already exists");
+    task_json.push(task_data);
+    return "Task created successfully";
+  };
+
+  // find and update the task data array based off task_data json object
+  updateTask = async (task_id, task_data) => {
+    const indexToUpdate = task_json.findIndex((task) => task.id.$oid === task_id);
+    if (indexToUpdate === -1) throw new Error("Error in updating task");
+    task_json[indexToUpdate] = task_data;
+    return "Task updated successfully";
+  };
+
+  removeTask = async (task_id) => {
+    const indexToRemove = task_json.findIndex((task) => task.id.$oid === task_id);
+    if (indexToRemove === -1) throw new Error("Error in deleting task");
+    task_json.splice(indexToRemove, 1);
+    return "Task deleted successfully";
+  };
 }
 
-async function getTask(task_id) {
-  return task_json.find((task) => task.id.$oid === task_id);
-}
-
-// add task data to task data json array if it does not exist
-async function createTask(task_data) {
-  if(task_data.hasOwnProperty("id") === false) throw new Error("Task id not found");
-  const task = await getTask(task_data.id.$oid);
-  if (task !== undefined) throw new Error("Task already exists");
-  task_json.push(task_data);
-  return "Task created successfully";
-}
-
-// find and update the task data array based off task_data json object
-async function updateTask(task_id, task_data) {
-  const indexToUpdate = task_json.findIndex((task) => task.id.$oid === task_id);
-  if (indexToUpdate === -1) throw new Error("Error in updating task");
-  task_json[indexToUpdate] = task_data;
-  return "Task updated successfully";
-}
-
-async function removeTask(task_id) {
-  const indexToRemove = task_json.findIndex((task) => task.id.$oid === task_id);
-  if (indexToRemove === -1) throw new Error("Error in deleting task");
-  task_json.splice(indexToRemove, 1);
-  return "Task deleted successfully";
-}
 
 module.exports = {
   getTask,
