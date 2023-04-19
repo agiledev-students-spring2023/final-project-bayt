a2b8fdfbbf0da42b66f466fd648c8cb4597c939b
dianaYepes <day256@nyu.edu>
2023-04-19 04:14:14 -0400

    updating profile fields for db

--- back-end/test/controllers/profile.controller.test.js
+++ back-end/test/controllers/profile.controller.test.js
@@ -6,7 +6,7 @@ describe('gets', function () {
     const req = { params: { username: 'badbunny' } };
     const res = { json: function (data) { this.data = data } };
     await gets(req, res);
-    const expectedProps = ['_id','username', 'email', 'role', 'telephone', 'rooms', 'password'];
+    const expectedProps = ['_id','username', 'email', 'role', 'lastname', 'houses', 'firstname'];
     const userProps = Object.keys(res.data);
     assert.deepStrictEqual(userProps, expectedProps);
   });
@@ -15,13 +15,13 @@ describe('gets', function () {
 describe('update', () => {
     describe('update', function () {
         it('should update user data for an existing username', async function () {
-          const req = { params: { username: 'badbunny' }, body: { telephone: '574-327-3449' } };
+          const req = { params: { username: 'badbunny' }, body: { lastname: 'Ocasio' } };
           const res = { send: function (data) { this.data = data } };
           await update(req, res);
           // Assert that the response data is not empty
           assert.notStrictEqual(res.data, undefined);
           // Assert that the response data contains the updated telephone number
-          assert.strictEqual(res.data.telephone, '574-327-3449');
+          assert.strictEqual(res.data.lastname, 'Ocasio');
         });
     });
 });
