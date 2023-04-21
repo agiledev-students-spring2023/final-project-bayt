880d4abced9f658640f4a3b96c3651aeeb190cae
jojo1042 <jojo.yang1042@gmail.com>
2023-04-21 16:25:37 -0400

    implementing db for rooms

--- back-end/src/services/home.service.js
+++ back-end/src/services/home.service.js
@@ -1,13 +1,29 @@
 let rooms_json = require('../json/rooms.json')
+const Room = require('../models/room.model.js')
+let getAllRooms, addRoom
 
-async function getAllRooms() {
-    return rooms_json;
+if(process.env.NODE_ENV === 'production') {
+    getAllRooms = async() => {
+        return Room.find({}).lean();
+    };
+    
+    addRoom = async(room) => {
+        // const newRoom = room;
+        // rooms_json.push(newRoom);
+        await Room.create(room)
+        return newRoom;
+    }
 }
-  
-async function addRoom(room) {
-    const newRoom = room;
-    rooms_json.push(newRoom);
-    return newRoom;
+
+else {
+    getAllRooms = async () => {
+        return rooms_json
+    }
+
+    addRoom = async(room) => {
+        const newRoom = room
+        rooms_json.push(newRoom)
+    }
 }
 
 module.exports = {
