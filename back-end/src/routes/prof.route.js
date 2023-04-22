04da4d53c551f0bc1b34c0d1b3b42b3c686019c6
dianaYepes <day256@nyu.edu>
2023-04-22 04:06:17 -0400

    updating user schema, handling profile pic for users

--- back-end/src/routes/prof.route.js
+++ back-end/src/routes/prof.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const prof_controller = require('../controllers/prof.controller.js');
 
 router.get('/:username', prof_controller.gets);
-router.post('/', prof_controller.store);
+router.post('/:username', prof_controller.store);
 router.put('/:username',prof_controller.update);
 
 module.exports = router;
