4e42ea091d0933327ba06bdc4ae5f728f25641f1
zion-off <ajz317@nyu.edu>
2023-04-28 09:09:13 -0400

    added transaction validation schema

--- back-end/src/routes/finances.route.js
+++ back-end/src/routes/finances.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const finances_controller = require("../controllers/finances.controller.js");
+const transactionValidation = require("../validations/transaction.validation.js");
 
 router.get("/", finances_controller.getAllTransactions);
-router.post("/", finances_controller.addTransaction);
+router.post("/", transactionValidation, finances_controller.addTransaction);
 
 module.exports = router;
---
+++ back-end/src/validations/transaction.validation.js
@@ -0,0 +1,23 @@
+const { body } = require('express-validator');
+
+const transactionValidation = [  body('paidOrRequesting')    .isString().withMessage('Paid or Requesting must be a string')    .notEmpty().withMessage('Paid or Requesting is required')    .isIn(['Paid', 'Requesting']).withMessage('Paid or Requesting must be either Paid or Requesting'),
+  body('amount')
+    .isNumeric().withMessage('Amount must be a number')
+    .notEmpty().withMessage('Amount is required')
+    .isFloat({ min: 0 }).withMessage('Amount must be a positive number'),
+  body('toOrFrom')
+    .isString().withMessage('To or From must be a string')
+    .notEmpty().withMessage('To or From is required')
+    .isIn(['to', 'from']).withMessage('To or From must be either to or from'),
+  body('user')
+    .isString().withMessage('User must be a string')
+    .notEmpty().withMessage('User is required'),
+  body('forWhat')
+    .isString().withMessage('For What must be a string')
+    .notEmpty().withMessage('For What is required'),
+  body('date')
+    .isDate().withMessage('Date must be a valid date')
+    .notEmpty().withMessage('Date is required')
+];
+
+module.exports = transactionValidation;
